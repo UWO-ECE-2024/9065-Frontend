ba9a1f61f1f69c4b7251f40a2318447dcb1d345d
Ross <528266131@qq.com>
2024-11-10 17:46:12 -0500

    feat: custom request method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,40 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { API_URL } from "@/common/config";
+import { CustomRequest } from "@/types/request-and-response";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const request = async ({
+  method = "GET",
+  path = "",
+  data,
+}: CustomRequest) => {
+  // const baseUrl = process.env.API_URL;
+  const baseUrl = API_URL;
+
+  let response;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  if (method === "GET") {
+    response = await fetch(`${baseUrl}${path}`, {
+      method,
+      headers,
+    });
+  } else {
+    response = await fetch(`${baseUrl}${path}`, {
+      method,
+      headers,
+      body: JSON.stringify(data),
+    });
+  }
+
+  const responseData = await response.json();
+  if (!responseData.data) {
+    throw new Error("No data found");
+  }
+  return responseData;
+};
---
+++ src/types/request-and-response.d.ts
@@ -0,0 +1,5 @@
+export type CustomRequest = {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  path: string;
+  data?: object;
+};
